d3c2191e2734f682911bec31ebe112d71e064be7
ibrahim khalil <momoncristo@gmail.com>
2025-11-08 01:47:17 +0100

    update: deploy

--- src/components/portfolio/AboutSection.tsx
+++ src/components/portfolio/AboutSection.tsx
@@ -44,7 +44,7 @@ const AboutSection = () => {
             <span className="gradient-text">About</span> Me
           </h2>
           <p className="text-muted-foreground max-w-3xl mx-auto text-lg leading-relaxed">
-            I'm Ibrahim Khalil, a computer science student passionate about software development and emerging technologies
+            iiI'm Ibrahim Khalil, a computer science student passionate about software development and emerging technologies
             . I'm eager to apply my technical and problem-solving skills to real-world, innovative projects. I specialize in React and mobile development, and I enjoy building modern, 
             user-friendly web and mobile applications that deliver seamless user experiences. en fr
           </p>
